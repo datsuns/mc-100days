0f4241ab3389604015509cc0ef52d2094c2f70f4
datsuns <the.skeleton7@gmail.com>
2025-03-30 17:29:33 +0900

    change: change origin 0 to 1

--- src/client/java/me/datsuns/mc100days/Days.java
+++ src/client/java/me/datsuns/mc100days/Days.java
@@ -2,14 +2,15 @@ package me.datsuns.mc100days;
 
 public class Days {
     public final long TIME_PER_DAY = 24000;
+    public final int origin = 1;
     public long n;
 
     public Days() {
-        this.n = 0;
+        this.n = origin;
     }
 
     public boolean tick(long timeOfDay) {
-        long d = timeOfDay / TIME_PER_DAY;
+        long d = (timeOfDay / TIME_PER_DAY) + origin;
         if (this.n != d) {
             this.n = d;
             return true;
