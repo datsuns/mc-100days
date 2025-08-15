ae2e2ce6f1eaf0f31ac9c1ca7bc30635c3eb4595
datsuns <the.skeleton7@gmail.com>
2025-08-15 11:25:49 +0900

    update modid
    use fabric template generator:
      https://fabricmc.net/develop/template/

---
+++ src/client/java/me/datsuns/mc100days/Mc100daysClient.java
@@ -0,0 +1,15 @@
+package me.datsuns.mc100days;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Mc100daysClient implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("mc100days");
+
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		LOGGER.info("Hello Fabric client!");
+	}
+}
